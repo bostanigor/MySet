08d08fe15d555cc21687fad5e5a257a4e63b1430
bostanigor <bostanigor@gmail.com>
2020-09-11 11:05:54 +0300

    Add Merge function

--- MySet/Program.cs
+++ MySet/Program.cs
@@ -35,6 +35,12 @@ namespace MySet
             _rights = rights.ToArray();
         }
 
+        public MySet(int[] lefts, int[] rights)
+        {
+            _lefts = lefts;
+            _rights = rights;
+        }
+
         public bool Contains(int value)
         {
             for (var i = 1; i < _lefts.Length; i++)
@@ -42,7 +48,69 @@ namespace MySet
                     return true;
             return false;
         }
-        
+
+        public MySet Merge(MySet other)
+        {
+            var newLefts = new List<int>{int.MinValue};
+            var newRights = new List<int>{int.MinValue};
+            int i_1 = 1;
+            int i_2 = 1;
+            MySet lastObject;
+            while (true)
+            {
+                var left_1 = this._lefts[i_1];
+                var left_2 = other._lefts[i_2];
+                var right_1 = this._rights[i_1];
+                var right_2 = other._rights[i_2];
+                int leftNew;
+                int rightNew;
+                if (left_1 < left_2)
+                {
+                    leftNew = left_1;
+                    if (right_1 < left_2)
+                        rightNew = right_1;
+                    else {
+                        rightNew = right_1 <= right_2 ? right_2 : right_1;
+                        i_2++;
+                    }
+                    i_1++;
+                }
+                else
+                {
+                    leftNew = left_2;
+                    if (right_2 < left_1)
+                        rightNew = right_2;
+                    else {
+                        rightNew = right_2 <= right_1 ? right_1 : right_2;
+                        i_1++;
+                    }
+                    i_2++;
+                }
+                newLefts.Add(leftNew);
+                newRights.Add(rightNew);
+                
+                if (i_1 == this._lefts.Length)
+                {
+                    for (; i_2 < other._lefts.Length; i_2++)
+                    {
+                        newLefts.Add(other._lefts[i_2]);
+                        newRights.Add(other._rights[i_2]);
+                    }
+                    break;
+                }
+                if (i_2 == other._lefts.Length)
+                {
+                    for (; i_1 < this._lefts.Length; i_1++)
+                    {
+                        newLefts.Add(this._lefts[i_1]);
+                        newRights.Add(this._rights[i_1]);
+                    }
+                    break;
+                }
+            }
+            return new MySet(newLefts.ToArray(), newRights.ToArray());
+        }
+
         IEnumerator IEnumerable.GetEnumerator()
         {
             return (IEnumerator) GetEnumerator();
@@ -97,8 +165,10 @@ namespace MySet
     {
         static void Main(string[] args)
         {
-            var set = new MySet("1..10,22,30..31");
-            foreach (var x in set)
+            var set1 = new MySet("1..10,22,28,30..31");
+            var set2 = new MySet("5..8,22,23,24,32..35");
+            var set3 = set1.Merge(set2);
+            foreach (var x in set3)
             {
                 Console.Write(x);
                 Console.Write(" ");
